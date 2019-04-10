d80b5f4ebe6d0aa77bb49b25531b051cc4ee8e99
x-ian <x-ian@x-ian.net>
2019-04-10 19:44:18 +0200

    version 9 from server plus simple fixes to reassign reviewers

---
+++ pagtest.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+    <head>
+        <title>PHP Pagination</title>
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+    </head>
+
+<?php
+    require_once 'paginate.php';
+ 
+    $conn       = new mysqli( '127.0.0.1', 'root', 'password', 'world' );
+ 
+    $limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 25;
+    $page       = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
+    $links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 7;
+    $query      = "SELECT City.Name, City.CountryCode, Country.Code, Country.Name AS Country, Country.Continent, Country.Region FROM City, Country WHERE City.CountryCode = Country.Code";
+ 
+    $Paginator  = new Paginator( $conn, $query );
+ 
+    $results    = $Paginator->getData( $page, $limit );
+?>
+    <body>
+        <div class="container">
+                <div class="col-md-10 col-md-offset-1">
+                <h1>PHP Pagination</h1>
+                <table class="table table-striped table-condensed table-bordered table-rounded">
+                        <thead>
+                                <tr>
+                                <th>City</th>
+                                <th width="20%">Country</th>
+                                <th width="20%">Continent</th>
+                                <th width="25%">Region</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+<?php for( $i = 0; $i < count( $results->data ); $i++ ) : ?>
+              	      	<tr>
+				<td><?php echo $results->data[$i]['Name']; ?></td>
+                		<td><?php echo $results->data[$i]['Country']; ?></td>
+                		<td><?php echo $results->data[$i]['Continent']; ?></td>
+                		<td><?php echo $results->data[$i]['Region']; ?></td>
+        		</tr>
+<?php endfor; ?>
+
+			</tbody>
+                </table>
+                </div>
+        </div>
+        </body>
+<?php echo $Paginator->createLinks( $links, 'pagination pagination-sm' ); ?>
+
+</html>
